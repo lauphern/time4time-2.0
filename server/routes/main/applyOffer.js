ae9a763f285e4b591e4be659f8b8c44e8e354ea7
lauphern <lau.p.hern@hotmail.com>
2020-04-12 01:32:45 +0200

    tested and fixed bugs in applyOffer.js file

--- server/routes/main/applyOffer.js
+++ server/routes/main/applyOffer.js
@@ -4,32 +4,27 @@ const router = express.Router();
 const { usersCollection, offersCollection } = require("../../utils/db");
 
 router.post("/apply", function (req, res, next) {
-  debugger
   //First, we retrieve the offer the user wants to apply to
   offersCollection
     .doc(req.body.offerId)
     .get()
     .then((offerSnap) => {
-      debugger
       let offerId = offerSnap.id;
       res.offer = { id: offerId, ...offerSnap.data() };
       next();
     })
     .catch(() => {
-      debugger
       res.json({ message: "Could not apply to the offer" });
     });
 });
 
 router.post("/apply", function (req, res, next) {
-  debugger
   //Secondly, we retrieve the user and make sure they have enough time
   //on the time wallet to apply to the offer
   usersCollection
     .doc(req.session.user.id)
     .get()
     .then((userSnap) => {
-      debugger
       let userId = userSnap.id;
       let user = { userId, ...userSnap.data() };
       if (user.timeWallet < res.offer.duration) {
@@ -38,27 +33,23 @@ router.post("/apply", function (req, res, next) {
         next();
       }
     })
-    .catch(err => {
-      debugger
+    .catch((err) => {
       res.json({ message: "Could not apply to the offer" });
     });
 });
 
 router.post("/apply", function (req, res) {
-  debugger
   //Lastly, we modify the status of the offer and update the userRequest field
   //with the username of the user applying to this offer
   res.offer.userRequest = req.session.user.username;
   res.offer.status = "Pending";
-  //TODO revisar que puedo hacer update en este objeto
-  offerSnap
+  offersCollection
+    .doc(req.body.offerId)
     .set(res.offer, { merge: true })
     .then(() => {
-      debugger
       res.end();
     })
     .catch(() => {
-      debugger
       res.json({ message: "Could not apply to the offer" });
     });
 });
